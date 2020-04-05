4129f0a13c76d074560d3444d1f5cfb4df7f131f
Kailash Sankar <ksankar@conviva.com>
2020-04-05 16:03:39 +0530

    no auth for appeals search, fow now

--- controllers/AppealController.js
+++ controllers/AppealController.js
@@ -59,7 +59,7 @@ exports.AppealStore = [
 ];
 
 exports.search = [
-  auth,
+  //auth, kept disabled for home page search, ideally we need another route for this
   function (req, res) {
     try {
       const parsedData = parseQueryData(req.body.query);
